795ed878954f22b2be74dbb6f173ac00b5a87fc8
Ashish.Bokil <ashish.bokil@sustlabs.com>
2023-04-10 17:20:29 +0530

    test

--- api/controllers/arithmeticController.js
+++ api/controllers/arithmeticController.js
@@ -18,7 +18,7 @@ exports.calculate = function(req, res) {
   };
 
   // Determine the 
-  //random comment1
+  //random comment2
 
   if (! req.query.operation) {
     throw new Error("Unspecified operation");
